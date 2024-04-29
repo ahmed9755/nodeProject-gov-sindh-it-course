12a5e3b2e49ca9b604dda103ed0541e9ad8dd4ec
Ahmed1965 <ahmedmuhammed786@gmail.com>
2024-04-28 18:48:38 -0700

    number Guess Game

---
+++ dist/project01_number_guessing-game/project01_number_guessing-game.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+await (console.log(chalk.bgBlueBright.bold("\n\t**********Welcome to Number Guess Game**********\t\n")));
+let guessNumber = Math.floor(Math.random() * 10);
+console.log(guessNumber);
+let userNumber = await inquirer.prompt([
+    {
+        name: "userGuess",
+        type: "number",
+        message: "Enter your guess number....",
+    }
+]);
+if (userNumber.userGuess === guessNumber) {
+    console.log(chalk.bgGreenBright.bold("\n\t**********Well done, You win**********\t\n"));
+}
+else {
+    console.log(chalk.bgRedBright.bold("\n\t**********Better Luck Next time....You lose**********\t\n"));
+}
