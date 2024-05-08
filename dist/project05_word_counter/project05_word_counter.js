eeb6958291b90107304931fcb1251574ba590b7f
Ahmed1965 <ahmedmuhammed786@gmail.com>
2024-05-08 06:18:17 +0500

    word counter

---
+++ dist/project05_word_counter/project05_word_counter.js
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+const wordCount = await inquirer.prompt([
+    {
+        name: "countWord",
+        type: "input",
+        message: "Enter a Sentence for word count..."
+    }
+]);
+const word = wordCount.countWord.split(" ").length;
+console.log(word);
